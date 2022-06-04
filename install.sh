cea74b1080912331258af3bf2eb0f69fa6310aac
Daniel Haupt <mail@danielhaupt.de>
2022-06-04 10:21:16 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -12,8 +12,8 @@ if [[ "${_projects[@]}" =~ (^| )${_workspace_project}($| ) ]]; then {
 } fi
 
 # Setup SSH key
-ssh-keyscan review.typo3.org >> "${HOME}/.ssh/known_hosts"
-if [[ ! -z "${USER_SSH_KEY}" ]]; then
-    echo "${USER_SSH_KEY}" | base64 --decode > "${HOME}/.ssh/id_rsa"
-    chmod 0600 "${HOME}/.ssh/id_rsa"
-fi
+#ssh-keyscan review.typo3.org >> "${HOME}/.ssh/known_hosts"
+#if [[ ! -z "${USER_SSH_KEY}" ]]; then
+#    echo "${USER_SSH_KEY}" | base64 --decode > "${HOME}/.ssh/id_rsa"
+#    chmod 0600 "${HOME}/.ssh/id_rsa"
+#fi
