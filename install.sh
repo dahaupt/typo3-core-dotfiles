1294e2b0b54587a8fe2a468eec9e23b10f92b2aa
Daniel Haupt <mail@danielhaupt.de>
2022-06-04 10:28:14 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,15 +1,7 @@
 #!/usr/bin/bash
 
-_projects=(
-    https://github.com/TYPO3/typo3/
-)
-
-_workspace_project="$(git -C "$GITPOD_REPO_ROOT" config --get remote.origin.url)";
-
-if [[ "${_projects[@]}" =~ (^| )${_workspace_project}($| ) ]]; then {
-  brew install drud/ddev/ddev
-  ddev start
-} fi
+brew install drud/ddev/ddev
+ddev start
 
 # Setup SSH key
 #ssh-keyscan review.typo3.org >> "${HOME}/.ssh/known_hosts"
